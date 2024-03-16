da1f8a8c6b44c5bf2d7693b9391afb4750952bfa
#SagaR <sagar.us78@gmail.com>
2024-03-17 00:10:22 +0600

    btn added

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,23 @@
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area, cca3 } = country;
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(true);
+  };
+
   return (
     <div className="country">
       <h3>Name : {name.common}</h3>
       <img src={flags.png} alt="" />
-      <p></p>
+      <p>Population : {population}</p>
+      <p>Area : {area}</p>
+      <p>Code : {cca3}</p>
+      <button onClick={handleVisited}>Visited</button>
+      {visited && "I have visited this Country"}
     </div>
   );
 };
+import { useState } from "react";
 import "./Country.css";
 export default Country;
